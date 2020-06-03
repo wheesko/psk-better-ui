ac13b29e9bf7a02ef00ea35239e548e9f7d588e4
wheesko <41743424+wheesko@users.noreply.github.com>
2020-06-03 11:33:49 +0300

    again

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express()
 const PORT = process.env.PORT || 5000;
+
+console.log(path.join(__dirname, '..'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use("*",(req, res) =>{
     res.send("<h1>Welcome to your simple server! Awesome right</h1>");
